f3d8d350b163f2887ef71eeb8ae19a4078260d7f
015346 <keguanghua@inner.czy.com>
2018-02-08 15:36:13 +0800

    123

---
+++ src/main/webapp/index/js/index.js
@@ -0,0 +1,16 @@
+new Vue({
+		el : '#app',
+		data : {
+			
+		},
+		methods : {
+		 handleOpen : function(key, keyPath) {
+		        console.log(key, keyPath);
+		      },
+		      
+		 handleClose : function(key, keyPath) {
+		        console.log(key, keyPath);
+		      }
+		}
+	})
+	

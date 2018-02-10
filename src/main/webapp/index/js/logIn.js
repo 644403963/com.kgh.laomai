d4a3f2d297f374a0ca18ca42b113d36eb02b05ea
Administrator <Administrator@CN-20160329JWPR>
2018-02-10 12:54:50 +0800

    645465

--- src/main/webapp/index/js/logIn.js
+++ src/main/webapp/index/js/logIn.js
@@ -2,10 +2,21 @@ new Vue({
 		el : '#app',
 		data : {
 			name : '',
-			password : '',
-			src:''
+			password : ''
 		},
+		//页面加载完成
+		mounted(){
+			this.setImg();
+		},
+		
 		methods : {
+			
+			setImg : function(){
+				$('.el-carousel__item:nth-child(3) img').attr('src','../../index/img/img1.jpg');
+				$('.el-carousel__item:nth-child(4) img').attr('src','../../index/img/img2.jpg');
+				$('.el-carousel__item:nth-child(5) img').attr('src','../../index/img/img3.jpg');
+				$('.el-carousel__item:nth-child(6) img').attr('src','../../index/img/img4.jpg');
+			},
 			check : function(event){
 				//获取值
 				var name = this.name;
